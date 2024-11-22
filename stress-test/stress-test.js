0afc32b696b04077214adc807823db23e8789c3e
nghia46 <nghialoe46@gmail.com>
2024-11-23 06:14:25 +0700

    update clean

--- stress-test/stress-test.js
+++ stress-test/stress-test.js
@@ -2,14 +2,14 @@ import http from "k6/http";
 import { check } from "k6";
 
 export let options = {
-  vus: 200, // Số lượng người dùng ảo đồng thời
+  vus: 300, // Số lượng người dùng ảo đồng thời
   duration: "5m", // Thời gian test (1 phút)
 };
 
 export default function () {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1000; i++) {
     // Mỗi người dùng gửi 10 yêu cầu
-    let res = http.get("http://localhost:8080/");
+    let res = http.get("http://10.99.15.249:8000/report");
     check(res, {
       "status is 200": (r) => r.status === 200,
     });
